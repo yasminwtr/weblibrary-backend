6f6ee5d9c1a1b911e31f4378375ba3ce3f1cf443
Yasmin Titow <yasmin.souza@allstrategy.com.br>
2025-06-11 22:00:40 -0300

    fix: origin cors

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ const fastify = Fastify({
 })
 
 fastify.register(cors, {
-    origin: ['https://weblibrary-frontend.vercel.app'],
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 })
